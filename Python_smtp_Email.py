597d735cee1e6e6f910926c9e4fb1ea10e4024de
Swethathangaraj <87110309+Swethathangaraj@users.noreply.github.com>
2021-10-02 19:14:44 +0530

    Python_smtp_Email 03102021

---
+++ Python_smtp_Email.py
@@ -0,0 +1,16 @@
+import smtplib as s
+ob=s.SMTP("smtp.gmail.com",587)
+
+ob.starttls()
+
+ob.login("trss1823@gmail.com","life goes on")
+
+subject="python message"
+body="this is tutorial of sending email using python script in simple steps"
+
+message="Subject:{}\n\n{}".format(subject,body)
+#print(message)
+listOfAddress=["monkeymindbusiness@gmail.com"]
+ob.sendmail("swetha1823",listOfAddress,message)
+print("send successfully....")
+ob.quit()
